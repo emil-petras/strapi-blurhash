16a68f9a39dbf8bdcf5646d32008b3f9cefe9bbe
Emil Petras <emil.petras@gmail.com>
2024-11-27 16:39:01 +0100

    updated to strapi v5

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,38 +1,47 @@
 'use strict';
 
-const { getAbsoluteServerUrl } = require('@strapi/utils');
-
 module.exports = ({ strapi }) => {
   const generateBlurhash = async (event, eventType) => {
+    console.log(`generating blurhash for ${eventType} event`);
     const { data, where } = event.params;
+
+    const host = strapi.config.get('server.host', 'defaultHost');
+    const port = strapi.config.get('server.port', 1337);
+    console.log(`server config - host: ${host}, port: ${port}`);
 
     if ((data.mime && data.mime.startsWith('image/'))) {
       let fullUrl = "";
       if (data.url.startsWith('http')) {
         fullUrl = data.url;
       } else {
-        fullUrl = `${getAbsoluteServerUrl(strapi.config)}${data.url}`;
+        fullUrl = `${"http://" + host + ":" + port}${data.url}`;
       }
+      console.log(`generating blurhash for image: ${fullUrl}`);
       data.blurhash = await strapi.plugin('strapi-blurhash').service('blurhash').generateBlurhash(fullUrl);
+      console.log(`blurhash generated successfully: ${data.blurhash}`);
     }
 
     if (eventType === 'beforeUpdate') {
       const regenerateOnUpdate = strapi.plugin('strapi-blurhash').config('regenerateOnUpdate');
       const forceRegenerateOnUpdate = strapi.plugin('strapi-blurhash').config('forceRegenerateOnUpdate');
+      console.log(`update config - regenerateOnUpdate: ${regenerateOnUpdate}, forceRegenerateOnUpdate: ${forceRegenerateOnUpdate}`);
     
       const fullData = await strapi.db.query('plugin::upload.file').findOne({
         select: ['url', 'blurhash', 'name', 'mime'],
         where
       });
+      console.log(`found existing file: ${fullData.name}`);
     
       if (fullData.mime.startsWith('image/') && (forceRegenerateOnUpdate || (!fullData.blurhash && regenerateOnUpdate))) {
         let fullDataUrl = "";
         if (fullData.url.startsWith('http')) {
           fullDataUrl = fullData.url;
         } else {
-          fullDataUrl = `${getAbsoluteServerUrl(strapi.config)}${fullData.url}`;
+          fullDataUrl = `${"http://" + host + ":" + port}${fullData.url}`;;
         }
+        console.log(`regenerating blurhash for image: ${fullDataUrl}`);
         data.blurhash = await strapi.plugin('strapi-blurhash').service('blurhash').generateBlurhash(fullDataUrl);
+        console.log(`blurhash regenerated successfully: ${data.blurhash}`);
       }
     }    
   };
@@ -42,4 +51,5 @@ module.exports = ({ strapi }) => {
     beforeCreate: (event) => generateBlurhash(event, 'beforeCreate'),
     beforeUpdate: (event) => generateBlurhash(event, 'beforeUpdate'),
   });
+  console.log('strapi-blurhash plugin bootstrap completed');
 };
